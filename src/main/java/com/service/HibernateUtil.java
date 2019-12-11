bdc2add72769f01af90426535759a5aebc909bfb
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2019-12-11 07:42:26 +0400

    update class HibernateUtil

--- src/main/java/com/service/HibernateUtil.java
+++ src/main/java/com/service/HibernateUtil.java
@@ -1,6 +1,7 @@
 package com.service;
 
 import com.entity.Person;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
