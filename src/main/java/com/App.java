a45c6ae49c4978a0665ee656869b291351975ee4
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2019-12-11 17:56:09 +0400

    update App

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,12 +1,21 @@
 package com;
 
 
+import com.dao.PersonDao;
+import com.entity.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         SpringApplication.run(App.class,args);
+
+
     }
 }
